931f22c6f9f9dd247575317b16659058059657b3
Alexisvnt <99617434+Alexisvnt@users.noreply.github.com>
2022-02-25 16:42:26 +0100

    Update App.py

--- App.py
+++ App.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt #permet de tracer et visualiser des données sou
 import math #fonction mathématique (RACINE)
 from pycaret.regression import * 
 import streamlit as st #permet de créer une application web
+import datetime
 #from PIL import Image
 
 def reading_dataset(): #fonction lire un jeu de données en excel ou csv
