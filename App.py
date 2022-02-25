fce66ede73c37300d5eb9e0d868925797bdac932
Alexisvnt <99617434+Alexisvnt@users.noreply.github.com>
2022-02-25 15:54:57 +0100

    Update App.py

--- App.py
+++ App.py
@@ -30,7 +30,7 @@ def calculconso(temps,intensiteh,cosphi):
 
 st.title('Wave concept estimation') #titre de l'application web
 
-st.sidebar.image(Image.open('logoalstefgroup.jpg'))
+#st.sidebar.image(Image.open('logoalstefgroup.jpg'))
 uploaded_file = st.sidebar.file_uploader("Upload le fichier excel", type=["xlsx"]) #Permet d'upload la liste des équipements sous format excel 
     
 st.sidebar.title('Parametres') #Titre parametres 
